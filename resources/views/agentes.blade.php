70ee9307fe928d22585a5416004f503444953ab0
yoshua <yoshuasoto12@outlook.com>
2020-08-03 14:04:00 -0400

    formularios

--- resources/views/agentes.blade.php
+++ resources/views/agentes.blade.php
@@ -26,15 +26,66 @@
       <!-- CONTENIDO AQUI -->
         <h1>Agregar Agente</h1>
         <br>
-          <form>
-            <div class="form-row">
+          <form action="" method="">
+            @csrf
+            <div class="row">
               <div class="col">
-                <input type="text" class="form-control" placeholder="First name">
+                <label for="exampleInputEmail1">Nombre</label>
+                <input type="text" class="form-control" placeholder="Nombre" name="name">
               </div>
               <div class="col">
-                <input type="text" class="form-control" placeholder="Last name">
+                <label for="exampleInputEmail1">Apellido</label>
+                <input type="text" class="form-control" placeholder="Apellido" name="last_name">
               </div>
             </div>
+            <br>
+            <div class="row">
+              <div class="col">
+                <label for="exampleInputEmail1">Rango</label>
+                <select class="form-control" name="rank">
+                  <option value="Clase Elite">Clase Elite</option>
+                  <option value="Clase Especial">Clase Especial</option>
+                  <option value="Clase 1">Clase 1</option>
+                  <option value="Clase 2">Clase 2</option>
+                  <option value="Clase 3">Clase 3</option>
+                </select>
+              </div>
+              <div class="col">
+                <label for="exampleInputEmail1">Número de Placa</label>
+                <input type="text" class="form-control" placeholder="000001" name="shell">
+              </div>
+            </div>
+            <br> 
+            <div class="row">
+              <div class="col">
+                <label for="exampleInputEmail1">Correo</label>
+                <input type="email" class="form-control" placeholder="Correo" name="email">
+              </div>
+              <div class="col">
+                <label for="exampleInputEmail1">Distrito Asignado</label>
+                <select class="form-control" name="distrit">
+                  <option value="Distrito Shibuya">Distrito Shibuya</option>
+                  <option value="Distrito Shinjuku">Distrito Shinjuku</option>
+                  <option value="Distrito Aoyama">Distrito Aoyama</option>
+                </select>
+              </div>
+            </div>
+            <br> 
+            <div class="row">
+              <div class="col-lg-6">
+                <label for="exampleInputEmail1">Código Sibyl</label>
+                <input type="text" class="form-control" placeholder="000001" name="cod_sibyl">
+              </div>
+            </div>  
+            <br>
+            <div class="row">
+              <div class="col">
+                <label for="exampleInputEmail1">Descripción</label>
+                <textarea class="form-control" cols="5" rows="5" placeholder="Descripción" name="description"></textarea>
+              </div>
+            </div>            
+            <br>                                
+            <button type="submit" class="btn btn-primary">Enviar</button>
           </form>        
     </div>
     <br>
@@ -44,14 +95,15 @@
         <table class="table">
           <thead class="thead-dark">
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
-              <th scope="col">Last</th>
-              <th scope="col">Acciones</th>              
+              <th scope="col">ID</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Apellido</th>
+              <th scope="col">Rango</th>
+              <th scope="col">Número de Placa</th>
+              <th scope="col">Correo</th>
+              <th scope="col">Distrito Asignado</th>
+              <th scope="col">Codigo Sibyl</th> 
+              <th scope="col">Descripción</th>             
             </tr>
           </thead>
           <tbody>
@@ -59,10 +111,11 @@
               <th scope="row">1</th>
               <td>Mark</td>
               <td>Otto</td>
-              <td>@mdo</td>
-              <td>Otto</td>
-              <td>@mdo</td>              
-              <td>Otto</td>
+              <td>Elite Class</td>
+              <td>00001</td>
+              <td>sibyl@sibyl.com</td>              
+              <td>Aoyama</td>
+              <td>N/A</td>
               <td><a class="btn btn-success" href="">Editar</a> <a class="btn btn-danger" href="">Eliminar</a></td>
             </tr>
           </tbody>
